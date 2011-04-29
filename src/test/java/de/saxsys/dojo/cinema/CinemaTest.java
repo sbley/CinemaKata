895a6dc3ebe05cfdbcc7ab83967897687f8c1076
Stefan Bley <stefan.bley@saxsys.de>
2011-04-29 13:20:08 +0200

    Kleine Änderungen am Test

--- src/test/java/de/saxsys/dojo/cinema/CinemaTest.java
+++ src/test/java/de/saxsys/dojo/cinema/CinemaTest.java
@@ -8,7 +8,8 @@ import org.junit.Test;
 public class CinemaTest {
 
 	@Test
-	public void shouldPay8forSawForOneAdult() throws Exception {
+	public void shouldPay800forSawForOneAdult() throws Exception {
+
 		final Cashier kasse = new Cashier();
 		kasse.startPurchase("Saw");
 		kasse.addTicket(30);
@@ -18,27 +19,28 @@ public class CinemaTest {
 
 	@Test
 	public void shouldPay550forSawForOneChild() throws Exception {
+
 		final Cashier kasse = new Cashier();
 		kasse.startPurchase("Saw");
 		kasse.addTicket(14);
 
 		assertThat(kasse.finishPurchase(), is(550));
-
 	}
 
 	@Test
 	public void shouldPay900forTitanicForOneAdult() throws Exception {
+
 		final Cashier kasse = new Cashier();
 		kasse.startPurchase("Titanic");
 		kasse.addTicket(15);
 
 		assertThat(kasse.finishPurchase(), is(900));
-
 	}
 
 	@Test
 	public void shouldPay4000forTitanicforThreeAdultsandTwoChildren()
 			throws Exception {
+
 		final Cashier kasse = new Cashier();
 		kasse.startPurchase("Titanic");
 		kasse.addTicket(30);
@@ -52,6 +54,7 @@ public class CinemaTest {
 
 	@Test
 	public void shouldPay6000forHarryPotterForTenAdults() throws Exception {
+
 		final Cashier kasse = new Cashier();
 		kasse.startPurchase("Harry Potter");
 		kasse.addTicket(30);
@@ -64,10 +67,13 @@ public class CinemaTest {
 		kasse.addTicket(30);
 		kasse.addTicket(30);
 		kasse.addTicket(30);
+
 		assertThat(kasse.finishPurchase(), is(6000));
 	}
+
 	@Test
 	public void shouldPay6600forSawForElevenAdults() throws Exception {
+
 		final Cashier kasse = new Cashier();
 		kasse.startPurchase("Saw");
 		kasse.addTicket(30);
@@ -81,6 +87,7 @@ public class CinemaTest {
 		kasse.addTicket(30);
 		kasse.addTicket(30);
 		kasse.addTicket(30);
+
 		assertThat(kasse.finishPurchase(), is(6600));
 	}
 }
