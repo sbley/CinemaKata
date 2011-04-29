e071cbac6ef560de50b625ecfe2704c0c79e02c9
Stefan Bley <stefan.bley@saxsys.de>
2011-04-29 17:30:11 +0200

    Lesbarkeit in Cashier erhoeht und magic numbers als Konstanten definiert

--- src/main/java/de/saxsys/dojo/cinema/Cashier.java
+++ src/main/java/de/saxsys/dojo/cinema/Cashier.java
@@ -2,39 +2,44 @@ package de.saxsys.dojo.cinema;
 
 public class Cashier {
 
-	private int totalPrice;
-	private String title;
+	private static final int RATE_CHILD = 550;
+	private static final int RATE_ADULT = 800;
+	private static final int GROUP_RATE = 600;
+	private static final int OVERTIME_RATE = 100;
+	private static final int MIN_GROUP_MEMBERS = 10;
+	private static final int MAX_AGE_CHILD = 14;
+
+	private int standardPrice;
+	private String movieTitle;
 	private int ticketCount;
 
-	void startPurchase(String title) {
-		this.title = title;
+	public void startPurchase(String movieTitle) {
+		this.movieTitle = movieTitle;
 	}
 
 	public void addTicket(int age) {
 		ticketCount++;
 
-		if (age > 14) {
-			totalPrice += 800;
+		if (age <= MAX_AGE_CHILD) {
+			standardPrice += RATE_CHILD;
 		} else {
-			totalPrice += 550;
+			standardPrice += RATE_ADULT;
 		}
 	}
 
 	public int finishPurchase() {
-		int price = totalPrice;
-		if (ticketCount >= 10) {
-			int discountedPrice = ticketCount * 600;
-			if (discountedPrice < totalPrice) {
-				price = discountedPrice;
-			}
+		int price = standardPrice;
+		if (ticketCount >= MIN_GROUP_MEMBERS) {
+			int groupPrice = ticketCount * GROUP_RATE;
+			price = Math.min(groupPrice, standardPrice);
 		}
 		if (isOvertime()) {
-			price += 100 * ticketCount;
+			price += ticketCount * OVERTIME_RATE;
 		}
 		return price;
 	}
 
 	private boolean isOvertime() {
-		return title.equals("Titanic");
+		return movieTitle.equals("Titanic");
 	}
 }
